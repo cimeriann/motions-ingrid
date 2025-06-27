bc52dc23d8aaca0add6f117f3dac3772c712790d
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2025-06-28 00:28:22 +0100

    feat: wrote auth/login controller

---
+++ src/features/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { AuthService } from './auth.service';
+import { loginSchema } from "./auth.validation";
+
+
+const authService = new AuthService();
+
+
+export class AuthController {
+	static async login(req: Request, res: Response, next: NextFunction){
+		try {
+			const validatedData = loginSchema.parse(req.body);
+			const token = await authService.login(validatedData.email, validatedData.password);
+			res.status(200).json({ token });
+		} catch (error) {
+			next(error)
+		}
+	}
+}
