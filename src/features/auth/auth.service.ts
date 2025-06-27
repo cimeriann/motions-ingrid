ffc5ff6874385841e92f172637720b4a7747cb29
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2025-06-28 00:29:01 +0100

    feat: wrote auth service

---
+++ src/features/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import prisma from '@utils/prisma';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import env from '@config/index';
+import { UnauthorizedError, NotFoundError } from '@middlewares/errorHandler';
+
+
+export class AuthService {
+	async login(email: string, password: string): Promise<string> {
+		const user = await prisma.user.findUnique({ where: {email}});
+
+		if (!user) {
+			throw new UnauthorizedError('Invalid credentials.')
+		}
+		const token = jwt.sign(
+			{id: user.id, email: user.email },
+			env.JWT_SECRET,
+			{ expiresIn: '1h'}
+		);
+
+		return token;
+	}
+}
