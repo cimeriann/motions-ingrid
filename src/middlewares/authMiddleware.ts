1631914a53b850ba75e763890d2130bf43b58fcd
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2025-06-29 23:35:05 +0100

    feat: wrote auth service

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken';
+import env from '@config/index';
+import { UnauthorizedError } from '@middlewares/errorHandler';
+import { Next } from "mysql2/typings/mysql/lib/parsers/typeCast";
+
+interface JwtPayload{
+	id: string;
+	email: string;
+}
+
+const authMiddleWare = ( req: Request, res: Response, next: NextFunction) => {
+	const authHeader = req.headers.authorization;
+
+	if(!authHeader || !authHeader.startsWith('Bearer ')){
+		return next(new UnauthorizedError('No token provided'));
+	}
+
+	const token = authHeader.split(' ')[1];
+
+	try {
+		const decoded = jwt.verify(token, env.JWT_SECRET) as JwtPayload;
+		req.user = decoded;
+		next();
+	}catch(error){
+		return next(new UnauthorizedError('Invalid or expired Token'));
+	}
+};
+
+export default authMiddleWare;
