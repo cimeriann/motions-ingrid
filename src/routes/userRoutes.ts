ce8ffd4c113e74d561f30af643ae0b7c02cc4ff5
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2025-06-29 23:35:49 +0100

    user routes

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,11 @@
+// import express from "express";
+// import { getAllUsers, getUserById, createUser } from "../controllers/userController";
+
+// const router = express.Router();
+
+
+// router.get("/", getAllUsers);
+// router.get("/:id", getUserById);
+// router.post("/", createUser);
+
+// export default router;
