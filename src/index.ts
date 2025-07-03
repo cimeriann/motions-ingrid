e0f3b272bf48dfb4df3982ee6c29ae80d9fa4c77
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2025-07-03 21:49:20 +0100

    fix: request body no showing up

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,41 @@
 import express from 'express';
+import env from '@config/index';
 import cors from 'cors';
 import helmet from 'helmet';
-import env from '@config/index'
-import userRouter from '@features/users/user.router';
-import authRouter from '@features/auth/auth.router'
-import errorHandler from '@middlewares/errorHandler';
 import { Request, Response, NextFunction } from 'express';
+import userRouter from '@features/users/user.router';
+import authRouter from '@features/auth/auth.router';
+import errorHandler from '@middlewares/errorHandler';
+// import { configDotenv } from 'dotenv';
+
+// configDotenv();
 
 const app = express();
 
+
 // Middleware
+// use body parser to parse JSON and URL-encoded data
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 const PORT = env.PORT || 3000;
 
+console.log(PORT)
 // routes
-
+// app.use((req, res, next) => {
+// 	console.log("req.body: ", req.body);
+// 	// res.status(200).json({message: 'this works' });
+// 	next();
+// })
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
 
-app.use((req, res, next) => {
-	res.status(404).json({message: 'Not Found' });
-})
-// Ensure errorHandler has the correct signature
-app.use(errorHandler as (err: Error, req: Request, res: Response, next: NextFunction) => void);
 
+app.use((req, res, next) => {
+	console.log("req.body: ", req.body);
+	res.status(404).json({message: 'Route Not Found' });
+})
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
