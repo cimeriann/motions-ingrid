776c800add518338d8c63ecd0b50fd8d7088e2e9
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2025-07-18 22:44:15 +0100

    feat: improve validators

--- src/features/users/user.validation.ts
+++ src/features/users/user.validation.ts
@@ -10,4 +10,4 @@ export const updateUserSchema = z.object({
 	name: z.string().min(3, 'Name must be at least 3 characters long'),
 	email: z.string().email('Invalid email address'),
 	password: z.string().min(6, 'Password must be at least 6 characters long')
-}).partial()	;
+}).partial();
